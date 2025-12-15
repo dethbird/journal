cd93249de26bb74ddb5fae184c8f5d93a8b35e4d
Rishi Satsangi <rishi.satsangi@gmail.com>
2025-12-15 23:21:21 +0000

    only one server now

--- src/api/server.js
+++
@@ -1,5 +0,0 @@
-import server from '../web/server.js';
-
-// This file simply starts the unified server (API + static UI).
-// Keeping it so `npm run api` still works as an alias.
-export default server;
